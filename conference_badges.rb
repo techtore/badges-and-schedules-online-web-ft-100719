43e88ba0c350204018e94457e35610d9433c99b4
Victoria Hale <vic.hale74@gmail.com>
2019-10-09 21:08:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+
+return "Hello, my name is #{name}."
+  
+end
+ 
+ def batch_badge_creator(array)
+   badges = []
+    array.each do |name| 
+    badges.push("Hello, my name is #{name}.")
+   end
+   return badges
+ end
+ 
+ def assign_rooms(speaker)
+  # use .collect to colect and store rooms assign strings
+   
+   
